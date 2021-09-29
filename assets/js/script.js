a11b607a0e228008b5f3c9fb14736a1bf879a345
Vinh Pham <phamv1@unlv.nevada.edu>
2021-09-29 00:08:13 -0700

    add js for coin flip, add msg panel

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,53 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Hi')
 
     // TODO: Add event listener and handler for flip and clear buttons
+    let coinFlipButton = document.querySelector('#flip-coin');
+    coinFlipButton.addEventListener('click', function () {
+        flipTheCoin();
+    })
+    let clearScoreButton = document.querySelector('#clear-score');
+
+    let coinImage = document.querySelector('#coin-image');
+
+    let coinFace = [ 
+        {   
+            name: 'heads', 
+            source: 'assets/images/penny-heads.jpg'
+        },
+        {   
+            name: 'tails',
+            source: 'assets/images/penny-tails.jpg'
+        }
+    ]
+
+    let scoreStat = {
+        heads: {
+            score: 0,
+            percentage: 0
+        },
+        tails: {
+            score: 0,
+            percentage: 0
+        },
+        totalFlip: 0
+    }
 
     // Flip Button Click Handler
         // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
+    function flipTheCoin () {
+       
+        let randomFlip = Math.floor(Math.random()*2);
+        //console.log(coinFace[randomFlip].name)
+        updateCoinImage(coinFace[randomFlip].source)
+        
+       calculateScore(coinFace[randomFlip].name);
 
+    }
+
+        // TODO: Update image and status message in the DOM
+    function updateCoinImage (imageSource) {
+        coinImage.setAttribute('src', imageSource);
+    }
         // Update the scorboard
         // TODO: Calculate the total number of rolls/flips
         // Make variables to track the percentages of heads and tails
@@ -19,9 +61,51 @@ document.addEventListener('DOMContentLoaded', function () {
         // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
         // TODO: Update the display of each table cell
 
+    function calculateScore (coinFace) {
+        scoreStat.totalFlip++;
+        scoreStat[coinFace].score++
+        
+        if(scoreStat.totalFlip > 0) {
+            
+            let percentage = Math.round(100*(scoreStat[coinFace].score/scoreStat.totalFlip))
+            //console.log(percentage)
+            scoreStat[coinFace].percentage = percentage;
+
+            document.querySelector(`#${coinFace}`).textContent = scoreStat[coinFace].score;
+            document.querySelector(`#${coinFace}-percent`).textContent = (scoreStat[coinFace].percentage).toString()+'%';
+
+            // recalculate the percentage for the coin face that didn't come up during the flip
+            if (coinFace == 'heads') {
+                scoreStat.tails.percentage = 100 - percentage;
+                document.querySelector(`#tails-percent`).textContent = ( scoreStat.tails.percentage).toString()+'%';
+            } else {
+                scoreStat.heads.percentage = 100 - percentage;
+                document.querySelector(`#heads-percent`).textContent = ( scoreStat.heads.percentage).toString()+'%';
+            }
+
+        }
+
+    }
+
 
     // Clear Button Click Handler
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
+    const clearButton = document.querySelector('#clear-score');
+    clearButton.addEventListener('click', function () {
+        scoreStat.heads.score = 0;
+        scoreStat.heads.percentage = 0;
+        scoreStat.tails.score = 0;
+        scoreStat.tails.percentage = 0;
+        scoreStat.totalFlip = 0
+
+        document.querySelector(`#tails-percent`).textContent = scoreStat.tails.percentage+'%';
+        document.querySelector(`#heads-percent`).textContent = scoreStat.heads.percentage+'%';
+        document.querySelector(`#tails`).textContent = scoreStat.tails.score;
+        document.querySelector(`#heads`).textContent = scoreStat.heads.score;
+        
+
+
+    })
 
 })
